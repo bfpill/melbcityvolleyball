cd2f3f939bd39608c04e66d482ef057a6d25680b
Max Adam <max@v3rv.com>
2023-03-17 08:42:18 +1100

    More images

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./About.css";
 import Container from "../components/Container";
-import groupPhoto from "../images/groupPhoto.JPG";
-import groupPhoto2 from "../images/groupPhoto2.JPG";
+import waupaca1 from "../images/waupaca1.jpg";
+import waupaca2 from "../images/waupaca2.jpg";
+import slipSlide from "../images/slipSide.jpg";
 
 function About() {
   return (
@@ -11,40 +12,24 @@ function About() {
         <h1 className="aboutText">
         Welcome to the first-ever organized grass volleyball tournament in Victoria, Australia!
         <br />
-         We are excited to bring this fantastic version of our favourite sport to Australia.
-        <br />
-        Our aim is to regularly host weekend tournaments and 
-          <br />
+        Get ready for an exciting day of 3 vs 3 grass volleyball action at the upcoming tournament in Princes Park, Melbourne, Victoria! 
         </h1>
-      </div>
+        </div>
+        
       <div className="imageContainer">
-        <img className="image" src={groupPhoto} />
+        <img className="image" src={slipSlide} />
       </div>
+
       <div className="colourContainer">
         <h1 className="aboutText">
-          Our mission is to provide a welcoming and inclusive environment where
-          everyone can enjoy the thrill of the game, make new friends, and
-          improve their skills.
-          <br />
-          <br />
-          We are excited to announce that we will soon be hosting our first
-          pickup event, which will provide a fantastic opportunity for our
-          members to come together and showcase their abilities. We are
-          committed to promoting the sport of volleyball and fostering a sense
-          of community, and we can't wait to see you on the court!
-          <br />
-          <br />
-          We believe that volleyball is more than just a sport; it's an
-          opportunity to build friendships, stay active, and have fun. Whether
-          you're a seasoned player or a beginner, we have programs and events
-          that cater to your needs and skill level. We are constantly seeking
-          new ways to improve our organization and provide the best possible
-          experience for our members. Join us at Melb City Volleyball and
-          discover the joy and excitement of this fantastic sport!
+        The tournament is open to mixed-gender teams, with players battling it out on a grass court using a men's net. Whether you're an experienced player or a beginner, this tournament is a fantastic opportunity to showcase your skills and experience the thrill of grass volleyball in a beautiful outdoor setting.
         </h1>
       </div>
       <div className="imageContainer">
-        <img className="image" src={groupPhoto2} />
+        <img className="image" src={waupaca1} />
+      </div>
+      <div className="imageContainer">
+        <img className="image" src={waupaca2} />
       </div>
     </>
   );
