18f7922b72c56ad1aa5367ea03c80b00122cd4ac
bfpill <max@v3rv.com>
2023-04-15 20:57:24 +1000

    Updated Register
    yeah yeah

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,25 +4,39 @@ import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 import { auth, db, logout } from "../firebaseConfig";
 import { query, collection, getDocs, where } from "firebase/firestore";
+import Home from "../pages/Home";
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
+  const [tournamentResults, setTournamentResults] = useState([]);
+
   const navigate = useNavigate();
-  const fetchUserName = async () => {
+
+  const fetchUserData = async () => {
     try {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
       const doc = await getDocs(q);
       const data = doc.docs[0].data();
+
+      const infoQ = query(collection(db, "playerInfo"), where("uid", "==", user?.uid));
       setName(data.name);
+      setRating(data.rating);
+      setTournamentResults(data.results);
     } catch (err) {
+        return(
+            <Home/>
+        )
       console.error(err);
     }
   };
+
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
-    fetchUserName();
+    fetchUserData();
   }, [user, loading]);
+
   return (
     <div className="dashboard">
        <div className="dashboard__container">
