e1e303c485e86a551e9c360782fb5f382e79aef8
bfpill <max@v3rv.com>
2023-04-16 22:32:48 +1000

    Register Page updated
    Also firebase is properly creating docs with userdata, had to update firebase rules in firestore online GUI

--- src/components/register.js
+++ src/components/register.js
@@ -11,19 +11,31 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [experience, setExperience] = useState("");
+
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
+
+  const handleExperienceChange = (event) => {
+    setExperience(event.target.value);
+  };
+
   const register = () => {
     if (!name) alert("Please enter name");
     registerWithEmailAndPassword(name, email, password);
   };
+
   useEffect(() => {
     if (loading) return;
     if (user) navigate("/dashboard");
   }, [user, loading]);
+
   return (
     <div className="register">
       <div className="register__container">
+        <div className="registerTitle">Create a Vic Grass Player Account:</div>
         <input
           type="text"
           className="register__textBox"
@@ -45,6 +57,79 @@ function Register() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
+        <input
+          type="text"
+          className="register__textBox"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          placeholder="Gender"
+        />
+        <input
+          type="text"
+          className="register__textBox"
+          value={age}
+          pattern="[0-9]*"
+          maxLength="2"
+          onChange={(e) => {
+            const charCode = e.which ? e.which : e.keyCode;
+            if (charCode < 48 || charCode > 57) {
+              e.preventDefault();
+            }
+            setAge(e.target.value)}
+          }
+
+          placeholder="Age"
+        />
+        <div className="register__skill__textbox">
+          {" "}
+          Select your skill level:
+        </div>
+        <div className="experience__options">
+          <label className="experience__option">
+            <input
+              type="radio"
+              name="experience"
+              value="beginner"
+              checked={experience === "beginner"}
+              onChange={handleExperienceChange}
+            />
+            <span className="experience__circle"></span>
+            <span className="experience__text">Beginner</span>
+          </label>
+          <label className="experience__option">
+            <input
+              type="radio"
+              name="experience"
+              value="intermediate"
+              checked={experience === "intermediate"}
+              onChange={handleExperienceChange}
+            />
+            <span className="experience__circle"></span>
+            <span className="experience__text">Intermediate</span>
+          </label>
+          <label className="experience__option">
+            <input
+              type="radio"
+              name="experience"
+              value="advanced"
+              checked={experience === "advanced"}
+              onChange={handleExperienceChange}
+            />
+            <span className="experience__circle"></span>
+            <span className="experience__text">Advanced</span>
+          </label>
+          <label className="experience__option">
+            <input
+              type="radio"
+              name="experience"
+              value="expert"
+              checked={experience === "expert"}
+              onChange={handleExperienceChange}
+            />
+            <span className="experience__circle"></span>
+            <span className="experience__text">Expert</span>
+          </label>
+        </div>
         <button className="register__btn" onClick={register}>
           Register
         </button>
